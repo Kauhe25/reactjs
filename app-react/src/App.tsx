c63e8164a570dd2ba0dc1f9f93c05f15b9515c8c
kauhe <kauhe25@gmail.com>
2024-11-05 19:56:26 -0300

    segunda parte

--- app-react/src/App.tsx
+++ app-react/src/App.tsx
@@ -3,6 +3,9 @@ import './App.css'
 function App() {
 
   const welcome = <h1>bem vindo, Kauhe</h1>
+  const gostando = true;
+  const numero = 1;
+  const cursos = ['html','css','js']
   return (
     <>
     {/* comentário ctrl + ; */}
@@ -10,6 +13,16 @@ function App() {
         <h1>Aula 01</h1>
         {welcome}
         <h2>Introdução ao React</h2>
+        {gostando? 'Estou curtindo' : 'Não estou curtindo'}
+        <p className={numero % 2? 'impar' : 'par'}>{numero}</p>
+      
+        {gostando && 
+          <div>
+            <h3>eitaaa curso top</h3>
+          </div>
+        }
+
+      {cursos.map(c => <p>{c}</p>)}
       </div>
       <div>xxxx</div>
     </>
