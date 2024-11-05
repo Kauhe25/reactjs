5c366472152287f4c337ffe73bbfc467c31c9e6c
kauhe <kauhe25@gmail.com>
2024-11-04 22:04:16 -0300

    aula 01: estrutura basica do react

---
+++ app-react/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  const welcome = <h1>bem vindo, Kauhe</h1>
+  return (
+    <>
+    {/* comentário ctrl + ; */}
+      <div>
+        <h1>Aula 01</h1>
+        {welcome}
+        <h2>Introdução ao React</h2>
+      </div>
+      <div>xxxx</div>
+    </>
+  )
+}
+
+export default App
